9bbf82cfa630ed34441a68473d191f4450ed2bc1
Mikah Ho <mikah.ho@student.tdsb.on.ca>
2026-04-28 17:01:41 +0000

    yippar

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,47 @@
+/**
+ * Lesson: Arrays
+ * Author: Mikah Ho
+ * Date Created: April 28, 2026
+ * Date Last Modified: April 28, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	//q1
+	public static int[] createIntArray() {
+
+		int[] arr = new int[5];
+
+		for (int i = 0; i < 5; i++) {
+			
+			arr[i] = i + 1;
+		}
+
+		return arr;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	//q2
+	public static String[] createArray(String word1, String word2, String word3, String word4) {
+
+		String[] arr = {word1, word2, word3, word4};
+
+		return arr;
+	} 
+
+	//q3
+	public static String getElement(int num, String[] arr) {
+
+		return arr[num];
 	}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+	//q4
+	public static void replaceElement(int num, double element, double[] arr) {
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+		arr[num] = element;
 
-	public static void q5() {
-		//Write question 5 code here
 	}
-
 }
